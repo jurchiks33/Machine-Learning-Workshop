5bc239736fa5239c8d3e32b6115b3db151811ba6
Jurchiks Jurchiks <jurisslisans@gmail.com>
2026-01-07 13:53:52 +0100

    add: implement update_company_stats script for beta calculation

---
+++ all_company_data_sector_betas/update_company_stats.py
@@ -0,0 +1,11 @@
+
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+from sklearn.linear_model import LinearRegression
+
+BENCHMARK = "SPY" #benchmark for beta calculation
+
